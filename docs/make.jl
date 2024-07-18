2229818e14262fb731e437f03a3a50a3c6fefee5
Santymax98 <132070198+Santymax98@users.noreply.github.com>
2024-07-18 15:28:26 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,40 +3,36 @@ using Documenter
 
 DocMeta.setdocmeta!(TwinCopulas, :DocTestSetup, :(using TwinCopulas); recursive=true)
 
-makedocs(
-    modules = [TwinCopulas],
-    authors = "Santiago Jimenez Ramos",
-    sitename = "TwinCopulas.jl",
-    format = Documenter.HTML(
-        canonical = "https://Santymax98.github.io/TwinCopulas.jl",
-        edit_link = "main",
-        assets = String[],
+makedocs(;
+    modules=[TwinCopulas],
+    authors="Santiago Jimenez Ramos",
+    sitename="TwinCopulas.jl",
+    format=Documenter.HTML(;
+        canonical="https://Santymax98.github.io/TwinCopulas.jl",
+        edit_link="master",
+        assets=String[],
     ),
-    pages = [
-        "Home" => ["index.md", "starting.md"],
+    pages=[
+        "Home" => ["index.md",
+                   "starting.md"]
         "Guide" => [
-            "Getting started" => "Copula_Sklar.md",
+            
+            "Getting starting" => "Copula_Sklar.md",
             "Archimedean Copulas" => "Archimedean/Archimedean_theory.md",
-            "Elliptical Copulas" => "Elliptical/Elliptical_theory.md",
-            "Extreme Value Copulas" => "Extreme/Extreme_Value_theory.md",
-        ],
-        "Available Models" => [
-            "Archimedean Copulas" => "Archimedean/Available_Archimedean_models.md",
-            "Elliptical Copulas" => "Elliptical/Available_Elliptical_models.md",
-            "Extreme Value Copulas" => "Extreme/Available_Extreme_models.md",
+            "Elliptical Copulas"  => "Elliptical/Elliptical_theory.md",
+            "Extreme Value Copulas"=>"Extreme/Extreme_Value_theory.md",
+            #"Empirical Copula" =>  "Empirical.md",
+            #"Dependence Measure" => "Dependence.md"       
+        ]
+        "Avaliable Models" => [
+            "Archimedean Copulas" => "Archimedean/Avaliable_Archimedean_models.md",
+            "Elliptical Copulas"  => "Elliptical/Avaliable_Elliptical_models.md",
+            "Extreme Value Copulas"=>"Extreme/Avaliable_Extreme_models.md",
         ]
     ],
 )
 
-deploydocs(
-    repo = "github.com/Santymax98/TwinCopulas.jl",
-    branch = "main",
-    devbranch = "main",
-    target = "site",
-    forcepush = true
-)
-
 deploydocs(;
     repo="github.com/Santymax98/TwinCopulas.jl",
-    devbranch="main",
+    devbranch="master",
 )
